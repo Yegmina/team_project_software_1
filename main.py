b4eaa94508c1b24e7a4588bddcbad6f00f626e2c
Yehor <te35603560@gmail.com>
2024-09-12 15:25:51 +0300

    initial_push

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-Hello World!
-Love_Yehor!
-Love_Noah!
-Love_DangTran!
-Love_Kien!!!
+class Game :
+
+
+    def __init__(self):
+        self.money = 10000000
+        self.infected_population = 1
+        self.public_dissatisfaction = 20
+        self.research_progress = 0
+        self.cure_cost = 30000
+        self.game_over = False
+        self.game_turn = 0
+
+    def start(self):
+        pass
+    def make_choice(self):
+        pass
+    def check_game_status(self):
+
+        if self.infected_population >= 100000:
+            print("The infection has spread globally. Game Over!")
+            self.game_over = True
+        elif self.public_dissatisfaction >= 100:
+            print("Public dissatisfaction has reached critical levels. Anarchy ensues. Game Over!")
+            self.game_over = True
+        elif self.research_progress >= 100:
+            print("The cure has been developed! You saved the world!")
+            self.game_over = True
+
+
+game1 = Game()
