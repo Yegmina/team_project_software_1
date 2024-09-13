74397f1f7ab4ea8dfbaea70f4dfa36c83248477b
Yehor <te35603560@gmail.com>
2024-09-13 14:38:27 +0300

    database name changed to flight_game

--- Yehor.py
+++ Yehor.py
@@ -23,7 +23,7 @@ def get_airport_coordinates(icao_code):
         connection = mariadb.connect(
             host='127.0.0.1',
             port=3306,
-            database='test',
+            database='flight_game',
             user='root',
             password='root',
             autocommit=True
