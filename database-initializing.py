02113bf5dbade37b2fdfcc6ece7b0212e9d40b56
acidentally <iCombozstrafe@gmail.com>
2024-09-16 21:19:02 +0300

    Update database-initializing SQL Host

--- database-initializing.py
+++ database-initializing.py
@@ -1,12 +1,12 @@
-import mysql.connector
+import mariadb
+import random
 
-connection = mysql.connector.connect(
+connection = mariadb.connect(
     host = 'localhost',
     user = 'root',
     database = 'flight_game',
     password = 'root',
     autocommit = True,
-    collation = 'utf8mb4_general_ci',
 )
 
 #<editor-fold desc = "MYSQL-cursor optimization">
@@ -16,42 +16,36 @@ def run(s) :
     cursor.execute(s)
     try :
         return cursor.fetchall()
-    except : pass
+    except :
+        return None
 #</editor-fold>
 
-#<editor-fold desc = "DATABASE CREATION">
-        ###------ DATABASE CREATION ------###
-try :
-    cursor.execute(f"CREATE DATABASE game_database;")
-    print(f'{"CREATING FILES":_^50}')
-    print(f"{'Successfully created neccessary game files' :-^50}")
-
-except :
-    print(f"{'Files have been created' :-^50}")
-        ###-------------------------------###
-#</editor-fold>
 
 #Each game' database should consist of 30 airports
 #Below is the table specifying which contients should have how many airports
 #AF - 7 ; AS - 7 ; EU - 6
 #NA - 5 ; OC - 3 ; SA & AN - 1
-#<editor-fold desc="IMPLEMENTATION OF THE GAME DATABASE CREATION">
-import random
-def create_game_database() :
-    continents = ('AF', 'AS', 'EU', 'NA', 'OC', 'SA', 'AN')
-    NoCountriesEachContinent = (7, 7, 6, 5, 3, 1, 1)
-    GameAirports = []
+#<editor-fold desc="IMPLEMENTATION OF INSERTING DATA INTO game_database DB">
 
-    for i in range(7) :
-        query = f"SELECT name FROM airport WHERE continent = '{continents[i]}'"
+import main
+#ICAO Code - Infected (Bool) - Closed (Bool) - Large airport
+
+GameAirports = []
+def create_game_database() :
+    continents = ('AF', 'AS', 'EU', 'NA', 'OC', 'SA')
+    NoCountriesEachContinent = (7, 7, 7, 5, 3, 1)
+
+    for i in range(6) :
+        query = (f"SELECT name FROM airport WHERE continent = '{continents[i]}'"
+                 f" AND type = 'large_airport'")
         customList = run(query)
+
+        print(len(customList))
 
         for num in range(NoCountriesEachContinent[i]) :
             airport = customList[random.randint(0, len(customList) - 1)][0]
             if airport not in GameAirports:
                 GameAirports.append(airport)
 
-    ##I
 
-    cursor.execute(f"USE game_database;")
 #</editor-fold>
