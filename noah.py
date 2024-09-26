cb2e972f2a448159ca5f5b94a3d265a03a5bafca
Kien-LeChi <chikienl@metropolia.fi>
2024-09-26 13:00:03 +0300

    Changed noah.py
    Signed-off-by: acidentally <iCombozstrafe@gmail.com>

--- noah.py
+++ noah.py
@@ -1,1 +1,7 @@
 ### Noah's Code Goes Here ###
+import database_manager as db
+infected_poplulation = 1
+infection_multiplyer = 2
+def infection_spread(local_infected_population, )
+    infected_population*infection_multiplyer
+    print(inefcted_population)
