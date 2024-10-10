c6e504839f464205d7f314f423907ead0e9ff834
acidentally <iCombozstrafe@gmail.com>
2024-10-10 23:37:44 +0300

    awd

--- testing.py
+++ testing.py
@@ -1,14 +1,11 @@
 import database_manager as db
-game_name = 'Noah4'
+import noah as nh
 
-game_id = db.run(f"SELECT id FROM saved_games WHERE input_name = '{game_name}';")[0][0]
-
-icao_code_list = db.run(f'SELECT airport_id '
-                        f'FROM airport_info '
-                        f'WHERE game_id = "{game_id}";')
-
-for i in range(0, len(icao_code_list)):
-       print(icao_code_list[i][0])
-
-
-print(game_id)
+count = db.run(f"SELECT COUNT(*) FROM airport_info "
+               f"LEFT JOIN airport ON airport.ident = airport_id "
+               f"WHERE continent = 'SA' "
+               f"AND infected = 0 "
+               f"AND closed = 0 "
+               f"AND game_id = 90;")[0][0]
+nh.print_all_icao_codes(90, 'EU')
+print(count)
